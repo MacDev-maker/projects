bb6fa7b7d6dd41491602fe28c7e5252c47c8c774
Mac <wmaciek56@gmail.com>
2025-07-19 11:03:09 +0200

    setting-up-plan-display

--- plan/scripts/my-plans.js
+++ plan/scripts/my-plans.js
@@ -1,3 +1,30 @@
-import {allPlans} from '../scripts/data.js';
+function renderPlansArray() {
+  let plans = JSON.parse(localStorage.getItem('allPlans')) || [];
 
-console.log(localStorage.getItem('allPlans'));
+  let plansHTML = '';
+  plans.forEach((plan) => {
+    const html = `
+    <div class="plan-container">
+      <div class="plan-name">
+        ${plan.name}
+      </div>
+      
+      <button class="show-plan" data-id="${plan.name}">Show</button>
+    </div>
+    <div class="js-plan-display" id=${plan.name} hidden>tak</div>`;
+    plansHTML += html;
+  });
+  document.querySelector('.js-plans-container')
+    .innerHTML = plansHTML;
+
+  document.querySelectorAll('.show-plan')
+    .forEach((button) => {
+      button.addEventListener('click' ,() => {
+        const buttonId = button.dataset.id;
+        
+        document.getElementById(`${buttonId}`)
+          .toggleAttribute('hidden');
+      });
+    });
+}
+renderPlansArray();
