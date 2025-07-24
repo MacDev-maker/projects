cc933386d26291922edf2c4224f901a87ada5e09
Mac <wmaciek56@gmail.com>
2025-07-24 16:23:20 +0200

    make-second-section-complete

--- plan/scripts/workout.js
+++ plan/scripts/workout.js
@@ -1,18 +1,24 @@
-const box = document.querySelector('.box');
-const caption = document.querySelector('.barbell-caption');
+const boxes = document.querySelectorAll('.box');
+const captions = document.querySelectorAll('.barbell-caption');
+const headerBenefits = document.querySelector('.header-benefits');
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
+      const caption = entry.target;
+      const box = caption.previousElementSibling;
+
       box.classList.add('show');
+
       setTimeout(() => {
-        caption.classList.add('color-change')
-      },2100);
+        caption.classList.add('color-change');
+      }, 1900);
+
+      observer.unobserve(caption);
     }
   });
 }, {
   threshold: 0.7 
 });
 
-
-observer.observe(caption);
+captions.forEach(caption => observer.observe(caption));
