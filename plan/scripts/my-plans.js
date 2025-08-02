04b16da262bcb3610b2645d8fbe958e3f12dbdc1
Mac <wmaciek56@gmail.com>
2025-08-02 11:43:02 +0200

    display-whole-plan

--- plan/scripts/my-plans.js
+++ plan/scripts/my-plans.js
@@ -3,8 +3,7 @@ function renderPlansArray() {
 
   let plansHTML = '';
   plans.forEach((plan, index) => {
-    let html = `
-    <div class="plan-container">
+    let html = `<div class="plan-container">
       <div class="plan-name">
         ${plan.name}
       </div>
@@ -12,67 +11,37 @@ function renderPlansArray() {
       <div>
         <button class="js-delete-plan" data-id="${plan.name}">Delete</button>
         <button class="js-show-plan" data-id="${plan.name}">Show</button>
-      </div>
-    </div>`;
+      </div>`;
 
-    plan.array.forEach((value) => {
+    let planHtml = `<div class="js-plan-display" id="${plan.name}" hidden>
+      <div class = "schedule row">
+      <div class="plan-features">
+        <div class="exercise">Exercise</div>
+        <div class="sets">Sets</div>
+        <div class="reps">Reps</div>
+        <div class="rir">Rir</div>
+        <div class="weight">Weight</div>      
+      </div>`;
+
+    plan.array.forEach((value, index) => {
       const {data} = value;
 
-      const planHtml = `<div class="js-plan-display" id="${plan.name}" hidden>
-        <div class = "schedule row">
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[0]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[1]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[2]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[3]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[4]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[5]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[6]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[7]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[8]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[9]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[10]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[11]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[12]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[13]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[14]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[15]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[16]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[17]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[18]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[19]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[20]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[21]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[22]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[23]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[24]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[25]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[26]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[27]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[28]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[29]}>
-      </div>
-      <div class = "row js-row">
-        <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[30]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[31]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[32]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[33]}>
-        <input class = "inputText js-outputText" type = "text" placeholder = ${data[34]}>
-      </div>
-        </div> 
-      </div>`;
-      html += planHtml;     
+      for(let i = 0; i < data.length; i += 5) {
+        planHtml += `<div class = "row js-row">
+          <input class = "inputText js-outputText js-exercise" type = "text" placeholder = ${data[i]}>
+          <input class = "inputText js-outputText" type = "text" placeholder = ${data[i + 1]}>
+          <input class = "inputText js-outputText" type = "text" placeholder = ${data[i + 2]}>
+          <input class = "inputText js-outputText" type = "text" placeholder = ${data[i + 3]}>
+          <input class = "inputText js-outputText" type = "text" placeholder = ${data[i + 4]}>
+        </div>`;
+      }
     });
+    planHtml += `</div></div>`;
+
+    html += planHtml;
+
+    html += `</div>`;
+
     plansHTML += html;
   });
 
